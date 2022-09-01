9fc1a3da0888405b4cfc35e02cea1b9242e09296
WONDO-k <wondok95@naver.com>
2022-09-01 23:01:34 +0900

    2022-09-01

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,23 +1,34 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 //컴포넌트 스캔이 아닌 자바코드로 직접 등록하는 방법
 @Configuration
 public class SpringConfig {
 
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean //스프링 빈에 등록하라는 의미
-    public MemberService memberService(){
+    public MemberService memberService() {
         return new MemberService(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+       // return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 
 }
